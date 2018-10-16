bfc5935be0465ae39129e0c6fa8a54af8af8af26
Jessica Y <desh365@hotmail.com>
2018-10-15 22:06:36 -0500

    default spotify

--- liri.js
+++ liri.js
@@ -31,7 +31,30 @@ switch (command) {
   case "spotify-this-song":
     const song = process.argv[3]
     if (!song) {
-      console.log("Put a song as the fourth argument to this command!")
+      spotify
+        .request("https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE")
+        .then(function(data) {
+          //  console.log(data.name)
+          // console.log(data.albums.artists)
+
+          const {
+            external_urls: { spotify: url },
+            name: title,
+            album: { name: albumtitle },
+            artists: {
+              0: { name: artist }
+            }
+          } = data
+          console.log(data)
+          console.log(`Artist: ${artist}`)
+          console.log(`Song title: ${title}`)
+          console.log(`Spotify URL: ${url}`)
+          console.log(`Album title: ${albumtitle}`)
+        })
+        .catch(function(err) {
+          console.error("Error occurred: " + err)
+        })
+
       return
     }
     spotify
@@ -47,39 +70,15 @@ switch (command) {
           }
         } = response.tracks.items[0]
         // Log out the information we got
-        console.log(`Spotify URL: ${url}`)
-        console.log(`Song title: ${title}`)
-        console.log(`Album title: ${albumtitle}`)
         console.log(`Artist: ${artist}`)
+        console.log(`Song title: ${title}`)
+        console.log(`Spotify URL: ${url}`)
+        console.log(`Album title: ${albumtitle}`)
       })
       .catch(function(err) {
         console.log(err)
       })
-    /*2. `node liri.js spotify-this-song '<song name here>'`
 
-   * This will show the following information about the song in your terminal/bash window
-     
-     * Artist(s)
-     
-     * The song's name
-     
-     * A preview link of the song from Spotify
-     
-     * The album that the song is from
-
-   * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   
-   * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-   
-   * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-   * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-   
-   * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-   * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-   * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).*/
     break
   case "movie-this":
     var movie = process.argv[3]
